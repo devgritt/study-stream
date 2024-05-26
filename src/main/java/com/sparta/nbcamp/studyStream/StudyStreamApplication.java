bfa21198994fe5fc0be2623d59db102958bd26e0
devgriit <devccth@gmail.com>
2024-05-26 12:45:25 +0900

    chore : 프로젝트 셋팅

---
+++ src/main/java/com/sparta/nbcamp/studyStream/StudyStreamApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcamp.studyStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyStreamApplication.class, args);
+    }
+
+}
